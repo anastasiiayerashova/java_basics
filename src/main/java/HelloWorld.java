60e08e96b84b08be41a4993c0b5fc3b04a4ac852
Anastasia Yerashova <yerashova.a@gmail.com>
2026-02-19 00:02:32 +0100

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public int sum (int a, int b) {
+        return a + b;
+    }
+    public void main (String[] args) {
+        int result = sum(5, 2);
+        System.out.println("Java\nIs\nGreat");
+        System.out.print("hello");
+        System.out.print(" ");
+        System.out.print("world");
+    }
+}
